72ce1401735b59095a53554bf683db51259e97b7
Amanda Oliveira <amand2oliv@gmail.com>
2021-05-06 22:53:42 -0300

    basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import NavBar from "components/NavBar";
+import Footer from "components/Footer";
+import DataTable from "components/DataTable";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Hello world!</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+          <h1 className="text-primary">Hello world!</h1>
+        <DataTable />
+        </div>
+        <Footer />
+    </>
   );
 }
 
